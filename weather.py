56fbda925790c872486d94ee45201ee437f1c9e7
Julia Kulesza <kuleszajulia1@gmail.com>
2018-12-16 23:09:23 +0100

    weather forecast and readme file

--- weather.py
+++ weather.py
@@ -1,40 +1,47 @@
 import argparse
+import datetime
 import json
-import sys
 import requests
-
-WEATHER_WEBSITE = "https://api2.climacell.co/v2/realtime"
-GEOLOCALIZATION_WEBSITE = "http://www.mapquestapi.com/geocoding/v1/address"
-
-try:
-    with open("apikey", "r") as f:
-        API_KEY = f.readline()
-    with open("geoKey", "r") as f:
-        GEO_KEY = f.readline()
-except IOError as er:
-    print("Unable to open file containing apikey - exiting")
-    sys.exit(1)
+import configparser
 
 
-def get_coordinates(location):
-    """ pobiera współrzędne na podstawie lokalizacji """
+def get_coordinates(website, apikey, location):
+    """
+    Pobiera współrzędne na podstawie lokalizacji
+    :param website: endpoint API do pobrania koordynatów na podstawie parametru location
+    :param apikey: klucz do autoryzacji w serwisie
+    :param location: lokalizacja w formie nazwy miasta lub adresu
+    :return: koordynaty w formie słownika
+    """
     location_data = {
-        "key": GEO_KEY,
+        "key": apikey,
         "location": location
     }
-    r = requests.get(GEOLOCALIZATION_WEBSITE, location_data)
+    r = requests.get(website, location_data)
     if r.status_code != 200:
         print("Unable to get location coordinates")
         return
     json_acceptable_string = r.text.replace("'", "\"")
     d = json.loads(json_acceptable_string)
-    coordinates = d["results"][0]["locations"][0]["latLng"]
-    print(location)
+    try:
+        coordinates = d["results"][0]["locations"][0]["latLng"]
+    except IndexError:
+        print('Unknown weather for the specified location: ' + location)
+        raise
     return coordinates
 
 
-def get_current_weather(longitude, latitude):
-    """ pobiera inf o aktualnej pogodzie dla podanje lokalizacji """
+def get_current_weather(website, apikey, endpoint, longitude, latitude):
+    """
+    Wyswietla aktualna pogode dla podanej lokalizacji
+    :param website: strona do pobrania informacji o aktualnej pogodzie dla określonych współrzędnych
+    :param apikey: klucz do autoryzacji w serwisie
+    :param endpoint: endpoint API strony
+    :param longitude:
+    :param latitude:
+    :return:
+    """
+    url = website + endpoint
     data = {
         "geocode": {
             "lon": longitude,
@@ -45,30 +52,108 @@ def get_current_weather(longitude, latitude):
             {
                 "name": "temp",
                 "units": "C"
+            },
+            {
+                "name": "feels_like",
+                "units": "C"
+            },
+            {
+                "name": "humidity"
+            },
+            {
+                "name": "wind_speed"
+            },
+            {
+                "name": "cloud_cover"
             }
         ]
     }
-    r = requests.post(WEATHER_WEBSITE, json.dumps(data), headers={"apikey": API_KEY, "Content-Type": "application/json"})
+    r = requests.post(url, json.dumps(data), headers={"apikey": apikey, "Content-type": "application/json"})
     if r.status_code != 200:
         print("Unable to get current weather information")
         return
     json_acceptable_string = r.text.replace("'", "\"")
-    d = json.loads(json_acceptable_string)
-    print(f'Current temp: {d["temp"]["value"]}')
+    return json.loads(json_acceptable_string)
 
 
-def get_weather_forecast():
-    """ pobiera inf o prognozie pogody na kolejny dzien """
-    pass
+def get_forecast_weather(website, endpoint, apikey, longitude, latitude, num_of_days):
+    """
+    Wyswietla prognoze pogody dla podanej lokalizacji
+    :param website: strona do pobrania informacji o aktualnej pogodzie dla określonych współrzędnych
+    :param endpoint: endpoint API strony
+    :param apikey: klucz do autoryzacji w serwisie
+    :param longitude:
+    :param latitude:
+    :param num_of_days: liczba dni, dla ktorych nalezy wyswietlic prognoze
+    :return:
+    """
+    url = website + endpoint
+    data = {
+        "lon": longitude,
+        "lat": latitude,
+        "num_days": num_of_days,
+        "unit_system": "si",
+        "fields": "temp,feels_like,humidity,wind_speed"
+    }
+    r = requests.get(url, params=data, headers={"apikey": apikey, "accept": "application/json"})
+    if r.status_code != 200:
+        print("Unable to get current weather information")
+        print(r.text)
+        return
+    json_acceptable_string = r.text.replace("'", "\"")
+    return json.loads(json_acceptable_string)
+
+
+def read_input_arguments():
+    """ Czyta argumenty wejsciowe """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--forecast-days", dest="num_days", action="store", default="0", help="get forecast max 3 days")
+    location_group = parser.add_mutually_exclusive_group()
+    location_group.add_argument("--coordinates", dest="coordinates", action="store", help="location coordinates X,Y")
+    location_group.add_argument("--address", dest="address", action="store", help="location address")
+    return parser.parse_args()
+
+
+def read_config_file(cfg_path="config.ini"):
+    """ Czyta plik konfiguracyjny """
+    config = configparser.ConfigParser()
+    config.read(cfg_path)
+    return config
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    location_group = parser.add_mutually_exclusive_group()
-    location_group.add_argument("--coordinates", dest="coordinates", action="store", help="location coordinates X,Y")
-    location_group.add_argument("--city", dest="city", action="store", help="location city")
-    location_group.add_argument("--address", dest="address", action="store", help="location address")
-    args = parser.parse_args()
-    coordinates = get_coordinates(args.city)
-    get_current_weather(coordinates["lng"], coordinates["lat"])
-
+    input_args = read_input_arguments()
+    config = read_config_file()
+    geolocalization_data = config['GEOLOCALIZATION']
+    weather_data = config['WEATHER']
+    coordinates = get_coordinates(website=geolocalization_data['website'], apikey=geolocalization_data['apikey'],
+                                  location=input_args.address)
+    print(f"Prognoza pogody dla: {input_args.address}")
+    if input_args.num_days == "0":
+        d = get_current_weather(website=weather_data['website'], apikey=weather_data['apikey'],
+                                endpoint=weather_data['realtime_endpoint'], longitude=coordinates["lng"],
+                                latitude=coordinates["lat"])
+        print("Aktualna pogoda")
+        print(f'Temperatura rzeczywista: {d["temp"]["value"]}')
+        print(f'Temperatura odczuwalna: {d["feels_like"]["value"]}')
+        print(f'Wilgotność powietrza: {d["humidity"]["value"]} %')
+        print(f'Prędkość wiatru: {d["wind_speed"]["value"]} m/s')
+        print(f'Zachmurzenie: {d["cloud_cover"]["value"]} %')
+    else:
+        d = get_forecast_weather(website=weather_data['website'], endpoint=weather_data['forecast_daily_endpoint'],
+                                 apikey=weather_data['apikey'], longitude=coordinates["lng"],
+                                 latitude=coordinates["lat"], num_of_days=int(input_args.num_days) - 1)
+        current_dt = datetime.datetime.now()
+        header = '{:12}|{:9}|{:9}|{:9}|{:10}|{:10}|{:10}|{:15}|{:13}' \
+            .format('Data', ' Tmp min', ' Tmp max', ' Odcz min', ' Odcz max', ' Wilg min', ' Wilg max',
+                    ' Pr wiatru min',
+                    'Pr wiatru max')
+        print(header)
+        print('-' * len(header))
+        for index, day in enumerate(d):
+            date = (current_dt + datetime.timedelta(days=index)).strftime("%Y-%m-%d")
+            print('{:12}|{:9}|{:9}|{:9}|{:10}|{:10}|{:10}|{:15}|{:13}'
+                  .format(date, day["temp"][0]["min"]["value"], day["temp"][1]["max"]["value"],
+                          day["feels_like"][0]["min"]["value"], day["feels_like"][1]["max"]["value"],
+                          day["humidity"][0]["min"]["value"], day["humidity"][1]["max"]["value"],
+                          day["wind_speed"][0]["min"]["value"], day["wind_speed"][1]["max"]["value"]))
